41562e75b1592be6939021dfe27ce1c511154259
haoxiong <zhouhaoit@163.com>
2025-07-17 17:07:25 +0800

    1

---
+++ AppChannelSDK.js
@@ -0,0 +1,58 @@
+(function (global) {
+  function AppChannelSDK(gameName) {
+    this.gameName = gameName;
+    this.callbackMap = {};
+    this.bindFlutterCallback();
+  }
+
+  AppChannelSDK.prototype.generateId = function () {
+    return Date.now() + '_' + Math.random().toString(36).substr(2, 6);
+  };
+
+  AppChannelSDK.prototype.bindFlutterCallback = function () {
+    var self = this;
+    window.onFlutterCallback = function (callbackId, result) {
+      var cb = self.callbackMap[callbackId];
+      if (cb) {
+        cb(result);
+        delete self.callbackMap[callbackId];
+      }
+    };
+  };
+
+  AppChannelSDK.prototype.invokeFlutterMethod = function (methodName, dataOrCallback, callback) {
+    var data = {};
+    var actualCallback;
+
+    if (typeof dataOrCallback === 'function') {
+      actualCallback = dataOrCallback;
+    } else if (dataOrCallback !== undefined) {
+      data = dataOrCallback;
+      actualCallback = callback;
+    }
+
+    var callbackId = actualCallback ? this.generateId() : null;
+
+    if (callbackId && actualCallback) {
+      this.callbackMap[callbackId] = actualCallback;
+    }
+
+    var payload = {
+      method: methodName,
+      data: data,
+      callbackId: callbackId,
+      gameName: this.gameName
+    };
+
+    if (window.CocosChannel && window.CocosChannel.postMessage) {
+      window.CocosChannel.postMessage(JSON.stringify(payload));
+    } else {
+      console.warn(this.gameName + " CocosChannel [" + methodName + "] is not available.");
+      if (actualCallback) actualCallback(null);
+    }
+  };
+
+  // 暴露为全局变量
+  global.AppChannelSDK = AppChannelSDK;
+
+})(window);
